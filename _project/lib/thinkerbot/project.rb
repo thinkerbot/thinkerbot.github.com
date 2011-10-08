151f5a82c243fabf208cbdd605dddf778f4af19d
Simon Chiang <simon.a.chiang@gmail.com>
2011-10-07 20:13:39 -0600

    add task to build rdoc

--- _project/lib/thinkerbot/project.rb
+++ _project/lib/thinkerbot/project.rb
@@ -26,6 +26,22 @@ module Thinkerbot
       "#{name}-#{version}.gem"
     end
 
+    def gem_spec(version)
+      gemfile = gem_file(version)
+      gemspec = log_sh "gem specification '#{gemfile}' --ruby --backtrace"
+      eval(gemspec)
+    end
+
+    def code_dir(version)
+      gemspec = gem_spec(version)
+      gemspec.full_name
+    end
+
+    def rdoc_dir(version)
+      gemspec  = gem_spec(version)
+      rdoc_dir = File.join(gemspec.name, gemspec.version.to_s, 'rdoc')
+    end
+
     def fetch(version, force=false)
       gemfile = gem_file(version)
 
@@ -39,6 +55,24 @@ module Thinkerbot
 
     def unpack(version)
       log_sh %Q{gem unpack '#{gem_file(version)}' --backtrace}
+    end
+
+    def build_rdoc(version, force=false)
+      gemspec = gem_spec(version)
+      codedir = code_dir(version)
+      rdocdir = File.expand_path rdoc_dir(version)
+
+      if !File.exists?(rdocdir) || force
+        FileUtils.rm_rf rdocdir
+        FileUtils.mkdir_p File.dirname(rdocdir)
+
+        Dir.chdir(codedir) do
+          files =  gemspec.files.select {|file| File.extname(file) == '.rb' }
+          files += gemspec.extra_rdoc_files
+          opts  =  gemspec.rdoc_options
+          log_sh "rdoc -o '#{rdocdir}' '#{opts.join("' '")}' '#{files.join("' '")}'".gsub("''", "")
+        end
+      end
     end
   end
 end
